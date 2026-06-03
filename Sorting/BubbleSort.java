007027b87602e914937620fc13e952156c013063
Immani Rama Venkata Sri Sai <saichowdaryimmani@gmail.com>
2026-06-03 15:37:16 +0530

    Create BubbleSort.java

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+class BubbleSort {
+
+    public void bubbleSort(int[] arr) {
+
+        int n = arr.length;
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            boolean swap = false;
+
+            for (int j = 0; j < i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+
+                    swap = true;
+                }
+            }
+
+            if (!swap) {
+                break;
+            }
+        }
+
+        System.out.println("After Using Bubble Sort:");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {13, 46, 24, 52, 20, 9};
+
+        System.out.println("Before Using Bubble Sort:");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println();
+
+        BubbleSort sorter = new BubbleSort();
+        sorter.bubbleSort(arr);
+    }
+}
