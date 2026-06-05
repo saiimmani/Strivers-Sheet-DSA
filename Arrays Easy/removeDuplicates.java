eaaa85b226940355d6b9ddd3a9bdf7809ab1f677
Immani Rama Venkata Sri Sai <saichowdaryimmani@gmail.com>
2026-06-05 12:09:47 +0530

    Update removeDuplicates.java

--- Arrays Easy/removeDuplicates.java
+++ Arrays Easy/removeDuplicates.java
@@ -3,12 +3,13 @@ class Solution {
        if(nums.length == 0) return 0;
        int i = 0;
        for(int j = 1; j<nums.length; j++){
-        if(nums[j] != nums[i]){
-            i++;
-            nums[i] = nums[j];
+        if(nums[j] != nums[i]){ // if i , j pointing to same element it will skip that 
+            i++; // increments by one  
+            nums[i] = nums[j]; // // save new arr 
         }
        } 
 
        return i+1; // index of need count increment 
     }
 }
+// Two Pointer Approach 
