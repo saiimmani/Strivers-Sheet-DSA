657d0c1dca148b51649a0addb563d18666a40be2
Immani Rama Venkata Sri Sai <saichowdaryimmani@gmail.com>
2026-06-09 10:17:59 +0530

    Create ValidAnagram.java

---
+++ Arrays Easy/ValidAnagram.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+       
+      /* char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        
+        // Sort both character arrays
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        
+        
+        return Arrays.equals(sChars, tChars); --> O(n log n)
+        */
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
+
+        for (char ch : s.toCharArray()) {
+            map1.put(ch, map1.getOrDefault(ch, 0) + 1);
+        }
+
+        for (char ch : t.toCharArray()) {
+            map2.put(ch, map2.getOrDefault(ch, 0) + 1);
+        }
+
+        return map1.equals(map2);
+    }
+    }
