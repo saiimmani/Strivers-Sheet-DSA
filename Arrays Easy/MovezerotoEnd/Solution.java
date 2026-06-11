1760bd80d881b19c2acef3348d72dbcbb38a7a1a
Immani Rama Venkata Sri Sai <saichowdaryimmani@gmail.com>
2026-06-11 10:54:33 +0530

    Refactor moveZeros method into Solution class

--- Arrays Easy/MovezerotoEnd/Solution.java
+++ Arrays Easy/MovezerotoEnd/Solution.java
@@ -1,27 +1,13 @@
-import java.util.*;
-
-public class Main
-{
-    public static void main(String[] args)
-    {
-        int[] arr = {1,0,2,0,3,4};
-
-        moveZeros(arr);
-
-        System.out.println(Arrays.toString(arr));
-    }
-
-    public static void moveZeros(int[] arr)
-    {
+class Solution {
+    public void moveZeroes(int[] nums) {
         int j = 0;
 
-        for(int i=0;i<arr.length;i++)
-        {
-            if(arr[i] != 0)   // arr[0] = 0 ignore that element
-            {
-                int temp = arr[i]; // swap i,j values 
-                arr[i] = arr[j];
-                arr[j] = temp;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] !=0 ){
+                
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
 
                 j++;
             }
